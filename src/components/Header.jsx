9349206d1c338f3f27a8a734a799c6d15403bc7d
Martin Odongo <martinwere14@gmail.com>
2025-01-02 20:22:32 +0300

    feat: made a part of the header section making it responsive across various screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <div>
-      <h1>My name is Martin</h1>
-    </div>
+    <header className= "flex flex-col md:flex-row items-center justify-between px-4 py-2 bg-gray-100">
+      <div className="flex items-center">
+        <h1 className="text-xl font-bold">Logo</h1>
+      </div>
+      <nav className='hidden sm:flex'>
+        <ul className="flex space-x-4">
+          <li><a href="#" className="hover:text-gray-500">Home</a></li>
+          <li><a href="#" className="hover:text-gray-500">Product</a></li>
+          <li><a href="#" className="hover:text-gray-500">About</a></li>
+          <li><a href="#" className="hover:text-gray-500">Contact</a></li>
+
+        </ul>
+      </nav>
+      <div className="sm:hidden">
+
+      </div>
+    </header>
   )
 }
 
